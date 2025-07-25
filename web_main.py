cc6a90f2f928de4025516a48231186597817b33e
InduchoodanMB <induchoodanmb7@gmail.com>
2025-07-25 16:31:28 +0530

    Update web_main.py

--- web_main.py
+++ web_main.py
@@ -61,7 +61,10 @@ def submit_answers():
         'matches': matches
     })
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 
